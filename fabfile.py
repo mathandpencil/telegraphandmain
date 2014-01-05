e74cde86595eb5dcdc4f8c537a2c5f69de52040a
Joseph Misiti <joseph.misiti@gmail.com>
2014-01-05 14:52:52 -0500

    updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,13 +6,13 @@ from fabric.api import put, run, settings, sudo
 from fabric.operations import prompt
 from fabric.contrib import django
 
-env.REMOTE_CODEBASE_PATH		= '/home/ubuntu/mathandpencil'
+env.REMOTE_CODEBASE_PATH		= '/home/ubuntu/telegraphandmain'
 env.VENDOR_PATH					= "/home/ubuntu/code"
 env.BUILD_FOLDER				= "/home/ubuntu/build"
 env.PIP_REQUIREMENTS_PATH		= 'config/requirements.txt'
 env.GUNICORN_PID_PATH			= os.path.join(env.REMOTE_CODEBASE_PATH, 'logs/gunicorn.pid')
 env.PROJECT_PATH				= os.path.dirname(os.path.abspath(__file__))
-env.MATHANDPENCIL_PATH				= "/home/ubuntu/mathandpencil/"
+env.TELEGRAPHANDMAIN_PATH				= "/home/ubuntu/telegraphandmain/"
 env.BRANCH_NAME					= 'master'
 env.user						= 'ubuntu'
 
@@ -25,10 +25,11 @@ else:
 	IS_WINDOWS = False
 
 env.roledefs = {
+	'production' : ['204.236.234.153'],
 }
 
 def bootstrap():
-	with cd(env.MATHANDPENCIL_PATH):
+	with cd(env.TELEGRAPHANDMAIN_PATH):
 		run("./manage.py bootstrap")
 
 def upgrade_ubuntu():
@@ -60,15 +61,15 @@ def tail():
 	
 def pip_install():
 	""" Install requirements.txt using python pip """
-	with cd(env.MATHANDPENCIL_PATH):
+	with cd(env.TELEGRAPHANDMAIN_PATH):
 		#sudo('chown -R ubuntu.ubuntu /home/ubuntu/.pip/')
 		sudo('pip install -r %s' % env.PIP_REQUIREMENTS_PATH)
 		
 def deploy():
 	""" Install requirements.txt using python pip """
-	with cd(env.MATHANDPENCIL_PATH):
+	with cd(env.TELEGRAPHANDMAIN_PATH):
 		run('git pull')
-		run('cp mathandpencil/local_settings.production mathandpencil/local_settings.py')
+		run('cp telegraphandmain/local_settings.production telegraphandmain/local_settings.py')
 	compress_assets()
 	transfer_assets()	
 	cleanup_assets()
@@ -76,14 +77,14 @@ def deploy():
 	
 	
 def make_directories():
-	sudo('touch /var/log/mathandpencil.log')
-	sudo('chmod -R 777 /var/log/mathandpencil.log')
+	sudo('touch /var/log/telegraphandmain.log')
+	sudo('chmod -R 777 /var/log/telegraphandmain.log')
 	sudo('mkdir -p /var/logs/gunicorn/')
 	sudo('chmod -R 777 /var/logs/gunicorn/')
 	
 def setup_supervisor():
-	with cd(env.MATHANDPENCIL_PATH):
-		put('config/supervisor_gunicorn.conf', '/etc/supervisor/conf.d/mathandpencil.conf', use_sudo=True)
+	with cd(env.TELEGRAPHANDMAIN_PATH):
+		put('config/supervisor_gunicorn.conf', '/etc/supervisor/conf.d/telegraphandmain.conf', use_sudo=True)
 		sudo('supervisorctl update')
 		sudo('supervisorctl reload')
 	
@@ -93,25 +94,26 @@ def build_all():
 	
 
 def configure_nginx():
-	with cd(env.MATHANDPENCIL_PATH):
-		put("config/nginx.mathandpencil.conf", "/usr/local/nginx/conf/sites-enabled/mathandpencil.conf", use_sudo=True)
-	sudo('service nginx restart')
+	with cd(env.TELEGRAPHANDMAIN_PATH):
+		put("config/nginx.telegraphandmain.conf", "/usr/local/nginx/conf/sites-enabled/telegraphandmain.conf", use_sudo=True)
+	sudo('service nginx stop')
+	sudo('service nginx start')
 	
 	
 def compress_assets(bundle=False):
 	"""
 		Use jammit to compress assets
 	"""
-	local('jammit -c assets.yml --base-url https://www.socialq.com --output static')
+	local('jammit -c assets.yml --base-url https://www.telegraphandmain.com --output static')
 	local('tar -czf static.tgz static/*')
 
 def transfer_assets():
 	"""
 		Transfer compressed assets to EC2
 	"""
-	run('mkdir -p %s/static/' % env.MATHANDPENCIL_PATH)
-	put('static.tgz', '%s/static/' % env.MATHANDPENCIL_PATH)
-	with cd(env.MATHANDPENCIL_PATH):
+	run('mkdir -p %s/static/' % env.TELEGRAPHANDMAIN_PATH)
+	put('static.tgz', '%s/static/' % env.TELEGRAPHANDMAIN_PATH)
+	with cd(env.TELEGRAPHANDMAIN_PATH):
 		run('tar -xzf static/static.tgz')
 		run('rm -f static/static.tgz')
 
